6b69523520a4239220bd5a683947267601a2aaf4
Vizente Freeman <vizente@leo8eleven.com>
2019-04-09 11:03:54 -0500

    small tweak

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          FIFA 19 Table
-        </header>
+        <header className="App-header">The FastModel FIFA 19 Table</header>
         <Layout />
       </div>
     );
